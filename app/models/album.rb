c0a18f7185970a4861201207eedaac98fe6e8ae3
Jonathan Yeong <jonathanyeong@gmail.com>
2014-12-03 17:58:47 +1100

    Add validation for artist id
    Check if artist id has an existing artist. If it doesn't then the
    validation will fail. This prevents the album model saving an album with
    an invalid artist id.
    
    Fixes issue #1. Note tests will fail because they don't currently
    handle this validation.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,12 @@
 class Album < ActiveRecord::Base
-	belongs_to :artist
+	belongs_to :artist 
 
-	validates :title, :artist_id, presence: true
+	validates :artist_id_exists, :title, :artist_id, presence: true
+
+  protected
+  def artist_id_exists
+    return false if Artist.find_by_id(self.artist_id).nil?
+  end
+
 end
+
